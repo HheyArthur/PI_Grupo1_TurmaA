0298a46bc048b3fc195048846aade374f568d991
DeniseAlmeida <98367238+DeniseAlmeida1303@users.noreply.github.com>
2022-11-15 18:24:30 -0300

    Inicio História
    Primeira parte da história da Taverna

--- App.java
+++ App.java
@@ -1,7 +1,33 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hola pessual**");
-        System.out.println("Denise Por aqui");
-        System.out.println("arnaldo");
-    }
+
+   static void iniciotaverna(String n) {
+      System.out.println(
+            "\n Um humano simples que após anos trabalhando como ajudante de cozinha, decidiu que iria abrir sua própria taverna "
+                  + " e então, decidiu estudar meios de abrir seu próprio negócio e como administrar. Anos se passaram "
+                  + " e agora ele é um homem bem-sucedido com várias tavernas espalhadas pela cidade de vento forte e agora vive humildemente em uma taverna simples aproveitando a vida."
+                  + "\n A história começa em uma noite calma e chuvosa, o som de uma porta abrindo se propaga no ar e com esse som que começa a nossa aventura.\n"
+                  + "\n Era em torno das 10 horas da noite, e um homem entra em uma taberna que está bem afastada da cidade dos ventos, ele vai ao balcão, porém não vê ninguém, bate na mesa e escuta no fundo.\n"
+                  + "\n- Já vai! \n"
+                  + "\n No fundo da estalagem vem um homem, bem-vestido e aparentemente de bem com a vida e ele se aproxima do novo cliente.\n"
+                  + "\n Chimbinha: Olá em que posso ajudar? \n"
+                  + "\n Desconhecido:  Me dê alguma coisa forte, hoje meu dia foi terrível.\n"
+                  + "\n Chimbinha: Vamos ver...\n "
+                  + "\n O balconista vira e procura algo entre vários tipos de bebidas.\n"
+                  + "\n Chimbinha: Aqui está, um bom e velho Licor Luminoso, feito de diversas frutas, ervas e temperos. É tipo um licor comum com uma glândula luminosa de um Besouro de Fogo.\n"
+                  + "\n Desconhecido: Se ela for tão boa que nem sua aparência!\n"
+                  + "\n O cliente vira a bebida em sua boca e em seguida pede outra e outra, porém continua sóbrio. \n"
+                  + "\n Chimbinha: Me diga, qual é o seu nome? Nunca vi você por aqui e, o que te deixa tão desconfortável ao ponto de se embriagar a essas horas?\n "
+                  + "\n Desconhecido : Me chamo Jogador e o motivo que estou me entupindo de bebida e porque eu estou praticamente falido.\n"
+                  + "\n Chimbinha : Como assim?\n"
+                  + "\n Jogador : Estava juntando umas pratas para abrir um negócio, contratei uma equipe de goblins operários e construir minha fábrica de armadura e estava tudo indo bem,"
+                  + "\n tinha meus funcionários e eu seria o gerente e cuidaria das finanças, porém deu tudo errado.\n"
+                  + "\n Chimbinha : Estava seguindo alguma teoria de administração?\n"
+                  + "\n Chimbinha: O que seria a teoria geral da administração?\n");
+
+   }
+
+   public static void main(String[] args) {
+      iniciotaverna("n");
+   }
 }
